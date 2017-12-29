22187ac05bb24d12ef933cd569968b313a061448
cancan <17611008558@163.com>
2017-12-29 09:29:09 +0800

    第二个main方法提交

--- src/java/MainTest.java
+++ src/java/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("易票");
     }
-	/*public static void main1(String [] cc){
+	public static void main1(String [] cc){
 		System.out.println("易票");
-	}*/
+	}
 }
