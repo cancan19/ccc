677ebc04c0e9f316b6bb85575f8073c78266db85
cancan <17611008558@163.com>
2017-12-29 09:24:15 +0800

    git提交测试

---
+++ src/java/MainTest.java
@@ -0,0 +1,13 @@
+package java;
+
+
+
+public class MainTest {
+ 
+	public static void main(String[] args) {
+		System.out.println("易票");
+    }
+	/*public static void main1(String [] cc){
+		System.out.println("易票");
+	}*/
+}
